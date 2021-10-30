f76614115e113e46d296330da661e85fcd438e92
CNSAKIB <c.nazmuzsakib@gmail.com>
2021-10-31 00:46:26 +0600

    env file added for firbase

--- src/components/Firebase/Firebase.config.js
+++ src/components/Firebase/Firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBrTq8ZM1YXS3uXLCIzlLq6EdQG2ImwXrQ",
-    authDomain: "holiday-planner7.firebaseapp.com",
-    projectId: "holiday-planner7",
-    storageBucket: "holiday-planner7.appspot.com",
-    messagingSenderId: "573238140138",
-    appId: "1:573238140138:web:1c30fa2c879ea7690c4b93"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 export default firebaseConfig;
